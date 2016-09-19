86f9f378e39f6604fc3f7d95fb966bd81fb608e1
Jelle Bens <jelle.bens@capgemini.com>
2016-09-19 13:03:29 +0200

    Decoupling services from UI

--- SharpSolutions.JIPA.SensorService/StartupTask.cs
+++ SharpSolutions.JIPA.SensorService/StartupTask.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Net.Http;
 using Windows.ApplicationModel.Background;
 using System.Diagnostics;
+using SharpSolutions.JIPA.Sensors;
+using Windows.System.Threading;
 
 // The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
@@ -12,12 +14,30 @@ namespace SharpSolutions.JIPA.SensorService
 {
     public sealed class StartupTask : IBackgroundTask
     {
-        public void Run(IBackgroundTaskInstance taskInstance)
+        private BackgroundTaskDeferral _Deferral;
+        BMP280 _Sensor;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
         {
-            
+            _Deferral = taskInstance.GetDeferral();
+
+
             Configuration config = Configuration.Load();
 
             Debug.WriteLine(config.DeviceId);
+
+            _Sensor = new BMP280();
+
+            await _Sensor.Initialize();
+
+            ThreadPoolTimer timer = ThreadPoolTimer.CreatePeriodicTimer(OnSensorTimerElapsedHandler, TimeSpan.FromSeconds(5));
+
+        }
+
+        private async void OnSensorTimerElapsedHandler(ThreadPoolTimer timer)
+        {
+            float temp = await _Sensor.ReadTemperature();
+            Debug.WriteLine(temp);
         }
     }
 }
---
+++ SharpSolutions.JIPA.Sensors/BMP280CalibrationData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpSolutions.JIPA.Sensors
+{
+    public class BMP280CalibrationData
+    {
+        public UInt16 T1 { get; set; }
+        public Int16 T2 { get; set; }
+        public Int16 T3 { get; set; }
+        public UInt16 P1 { get; set; }
+        public Int16 P2 { get; set; }
+        public Int16 P3 { get; set; }
+        public Int16 P4 { get; set; }
+        public Int16 P5 { get; set; }
+        public Int16 P6 { get; set; }
+        public Int16 P7 { get; set; }
+        public Int16 P8 { get; set; }
+        public Int16 P9 { get; set; }
+    }
+}
